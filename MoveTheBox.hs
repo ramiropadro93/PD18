cb39c914ba470f4f2456575fc35973c4a255f9ea
ramiropadro93 <ramipadro93@gmail.com>
2018-12-02 14:44:07 -0300

    Agregados
    Sistema de niveles
    Limpieza de pantalla para hacer juego más amigable a la vista

--- MoveTheBox.hs
+++ MoveTheBox.hs
@@ -1,159 +1,150 @@
 import Prelude hiding (Either(..))
-import Data.List (sort,delete)
-import System.IO (stdin, stdout, hSetEcho, hSetBuffering, BufferMode(..))
+import System.IO  (stdin, stdout, hSetEcho, hSetBuffering
+                  ,BufferMode(..))
+import Control.Monad (forM_, liftM)    
+import Data.List (sort, delete, unfoldr)           
+
+data Input  = Up 
+            | Down 
+            | Left 
+            | Right
+            deriving (Show, Eq, Ord)
 
 type Coord = (Int, Int)
 
 data Map = Map  {wWalls
                 ,wBoxes
-                ,wStorages   :: [Coord]
+                ,wStorages  :: [Coord]
                 ,wWorker    :: Coord
                 ,wMax       :: Coord
                 ,wSteps     :: Int
-                } deriving (Show)
-
-emptyMap = Map  {wWalls      = []
-                ,wBoxes      = []
-                ,wStorages   = [] 
-                ,wWorker     = (0,0)
-                ,wMax        = (0,0)
-                ,wSteps      = 0
                 }
 
-data Input  = Up
-            | Down
-            | Left
-            | Right
-            deriving (Show, Eq, Ord)
+emptyMap = Map  {wWalls    = []
+                  ,wBoxes   = []
+                  ,wStorages = []
+                  ,wWorker   = (0,0)
+                  ,wMax      = (0,0)
+                  ,wSteps    = 0
+                  }
 
+instance Show Map where
+  show w = unlines chars
+    where (maxX, maxY)  = wMax w
+          chars         = [[func (x,y)  | x <- [0..maxX]] 
+                                        | y <- [0..maxY]]
+          func c 
+            | isBox   w c && isStorage w c  = '*'
+            | isWorker  w c && isStorage w c  = 'p'
+            | isWall    w c                   = '#'
+            | isWorker  w c                   = 'P'
+            | isBox   w c                   = 'o'
+            | isStorage w c                   = '.'
+            | otherwise                       = ' '
 
---carga de coordenadas. 
-add :: Coord -> Input -> Coord
-add (x,y) input =
-    case input of
-        Up      -> (x   , y-1)
-        Down    -> (x   , y+1)
-        Left    -> (x-1 , y)
-        Right   -> (x+1 , y)
-
-
---CONSTRUCCION DE NIVELES - LA IDEA ES HACERLO A PARTIR DE UN ARCHIVO
---La idea es tener una lista de niveles creando un sistema de niveles controlado automaticamente a medida que el juego avanza. FALTA HACER
-
---seleccion de nivel: 
-level = unlines
-    ["|||||||||||||||"
-    ,"|.. o   o     |"
-    ,"|..           |"
-    ,"|             |"
-    ,"|oo           |"
-    ,"|             |"
-    ,"|  P          |"
-    ,"|             |"
-    ,"|             |"
-    ,"|             |"
-    ,"|||||||||||||||"
-    ]
-
-
-
---carga de nivel
-loadLevel :: String -> Map
-loadLevel str = foldl consume (emptyMap{wMax = maxi}) elems
-    where   lns     = lines str
-            coords  = [[(x,y) | x <- [0..]] | y <- [0..]]
-            elems   = concat $ zipWith zip coords lns
-            maxX    = maximum . map (fst . fst) $ elems
-            maxY    = maximum . map (snd . fst) $ elems
-            maxi    = (maxX,maxY)
-            consume mapp (c,element) = case element of
-                'P' -> mapp{wWorker  = c}
-                'o' -> mapp{wBoxes   = c:wBoxes mapp}
-                '|' -> mapp{wWalls   = c:wWalls mapp}
-                '.' -> mapp{wStorages = c:wStorages mapp}
-                ' ' -> mapp
-                otherwise -> error (show element ++ " not recognized")
-
-
---mustra el mapa
-showMap :: Map -> IO()
-showMap w = putStrLn . unlines . map (map func) $ coords
-    where   (maxX, maxY)    = wMax w
-            coords          = [[(x,y) | x <- [0..maxX]] | y <- [0..maxY]]
-            isWorker  w c   = wWorker w == c
-            func c  | isBox     w c && isStorage w c    = '*'
-                    | isWorker  w c && isStorage w c    = 'P'
-                    | isWall    w c                     = '|'
-                    | isWorker  w c                     = 'P'
-                    | isBox     w c                     = 'o'
-                    | isStorage w c                     = '.'
-                    | otherwise                         = ' '
-                
---funcion principal, maneja la modificacion del mapa con cada movimiento
-modifyMap :: Map -> Input -> Map
-modifyMap map input
-    | isWall    map newPos    = map'
-    | isBox   map newPos    =
-        if isBox map newPos' || isWall map newPos'
-            then map'
-            else moveBox map' newPos newPos'
-    | otherwise                 = map'
-    where   moveBox w old new = w{wBoxes = new:delete old (wBoxes map)}
-            oldPos  = wWorker map
-            newPos  = add oldPos input
-            newPos' = add newPos input
-            map'  = map{wWorker = newPos, wSteps = wSteps map + 1}
-
---ingreso de teclas
 getInput :: IO Input
 getInput = do
-    char <- getChar
-    case char of
-        'w' -> return Up
-        'a' -> return Left
-        's' -> return Down
-        'd' -> return Right
-        otherwise -> getInput
+  char <- getChar
+  case char of
+    'w' -> return Up
+    'a' -> return Left
+    's' -> return Down
+    'd' -> return Right
+    otherwise -> getInput
 
---Encontre pared
+add :: Coord -> Input -> Coord
+add (x,y) input = 
+  case input of
+    Up    -> (x  , y-1)
+    Down  -> (x  , y+1)
+    Left  -> (x-1, y  )
+    Right -> (x+1, y  )
+
 isWall :: Map -> Coord -> Bool
-isWall map coord = elem coord (wWalls map) 
+isWall map coord = elem coord (wWalls map)
 
---Encontré caja
 isBox :: Map -> Coord -> Bool
 isBox map coord = elem coord (wBoxes map)
 
---Punto de extraccion
 isStorage :: Map -> Coord -> Bool
 isStorage map coord = elem coord (wStorages map)
 
---Nivel terminado
+isWorker :: Map -> Coord -> Bool
+isWorker w c  = wWorker w == c
+
+
+---sistema de niveles - se parsea el txt esperando linea en blanco para diferencia de un nivel a otro
+loadLevels :: String -> IO [Map]
+loadLevels filename = do
+  lns <- liftM lines . readFile $ filename
+  return $ unfoldr consume lns
+  where isEmptyLine = all (' '==)
+        consume [] = Nothing
+        consume ls = let (a,b) = break isEmptyLine ls
+                     in return (loadLevel $ unlines a, drop 1 b)
+
+--carga el nivel actual
+loadLevel :: String -> Map
+loadLevel str = foldl consume (emptyMap{wMax = maxi}) elems
+  where lns     = lines str
+        coords  = [[(x,y) | x <- [0..]] | y <- [0..]]
+        elems   = concat $ zipWith zip coords lns
+        maxX    = maximum . map (fst . fst) $ elems
+        maxY    = maximum . map (snd . fst) $ elems
+        maxi    = (maxX, maxY)
+        consume wld (c, elt) = 
+          case elt of
+            'P' -> wld{wWorker    = c}
+            'o' -> wld{wBoxes    = c:wBoxes wld}
+            '#' -> wld{wWalls     = c:wWalls wld}
+            '.' -> wld{wStorages  = c:wStorages wld} 
+            '*' -> wld{wBoxes    = c:wBoxes wld
+                      ,wStorages  = c:wStorages wld}
+            'p' -> wld{wStorages  = c:wStorages wld
+                      ,wWorker    = c}
+            ' ' -> wld
+            otherwise -> error (show elt ++ " not recognized")
+
+--funcion principal, modifica el mapa en cada movimiento del jugador
+modifyMap :: Map -> Input -> Maybe Map
+modifyMap map input 
+  | isWall    map newPos  = Nothing
+  | isBox   map newPos  = 
+      if isBox map newPos' || isWall map newPos'
+        then Nothing
+        else return $ moveCrate map' newPos newPos'
+  | otherwise               = return map'
+  where moveCrate w old new = w{wBoxes = new:delete old (wBoxes w)}
+        
+        oldPos  = wWorker map
+        newPos  = add oldPos input
+        newPos' = add newPos input
+        map'  = map{wWorker = newPos, wSteps = wSteps map + 1}
+        
+--nivel terminado
 isFinished :: Map -> Bool
-isFinished map = sort (wBoxes map) == sort (wStorages map)
+isFinished map = 
+  sort (wBoxes map) == sort (wStorages map)
 
-isValid :: Map -> Input -> Bool
-isValid map input   | isWall    map newPos  = False
-                    | isBox     map newPos  = not(isBox map newPos') && not(isWall map newPos')
-                    | otherwise             = True
-    where   oldPos = wWorker map  
-            newPos  = add oldPos input
-            newPos' = add newPos input
-
-main :: IO()
-main = do
---bloquea al sistema para evitar que se muestren los inputs (w-a-s-d)
-    hSetEcho stdin False
-    hSetBuffering stdin NoBuffering
-    hSetBuffering stdout NoBuffering
-
-    gameLoop $ loadLevel level
-
+--funcion de manejo del juego
 gameLoop map = do
-    showMap map
-    input <- getInput
-    let map' = if isValid map input
-                    then modifyMap map input
-                    else map
-    if isFinished map'
-        then showMap map' >> print "GOOD JOB WORKER !!" 
-        else gameLoop map'
+  putStr "\ESC[2J"
+  print map
+  input <- getInput
+  let map' = case modifyMap map input of
+                  Just x  -> x
+                  Nothing -> map
+  if isFinished map'
+    then putStr "\ESC[2J" >> print map'
+    else gameLoop map'  
+
+main :: IO ()
+main = do
+  hSetEcho stdin False
+  hSetBuffering stdin NoBuffering
+  hSetBuffering stdout NoBuffering
+
+  levels <- loadLevels "sokoban_levels.txt"
+  forM_ (zip [1..] levels) $ \(n,l) -> do
+    putStrLn $ "\n\nLevel " ++ show n ++ ":"
+    gameLoop l
